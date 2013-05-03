822b8742be1a7a19b00d23d6dd741d1e5b044fc4
Triaji Putranto <foreward@gmail.com>
2013-05-03 20:53:05 +0800

    Task 1 Question 1
    The program would detect if the start range and end range are positive
    integers. if not, the program would display warning messages.
    
    If both ranges are positive integers, then the core of the program will
    start. A for loop is used to go through the range of the numbers and
    PHP modulus is used to check if the number is either a multiple of 3
    which the program would print "Fizz" or if its a multiple of 5, it
    would print "Buzz" and if it is not either a multiple of 3 and not
    multiple of 5 then it would print the integer itself.
    
    Assumption, if the number is a multiple of 3 and 5 (eg. 15), the script
    would display both the Fizz and Buzz together making FizzBuzz

---
+++ task1.php
@@ -0,0 +1,60 @@
+<?php
+function buzzFizz($startRange, $endRange){
+	$valid = true;
+	if (!is_numeric($startRange)) 
+	{
+		$valid = false;
+		echo "- Start Range has to be an integer \n";
+	}
+	else if ($startRange < 0) 
+	{
+		$valid = false;
+		echo "- Start Range has to be a positive integer \n";
+	}
+	
+	if (!is_numeric($endRange)) 
+	{
+		$valid = false;
+		echo "- End Range has to be an integer \n";
+	} 
+	else if ($endRange < 0) 
+	{
+		$valid = false;
+		echo "- End Range has to be a positive integer \n";
+	}
+	
+	if ($valid)
+	{
+		$start = $startRange;
+		$end = $endRange;
+		if ($endRange < $startRange){
+			$start = $endRange;
+			$end = $startRange;
+		}
+		$result = "";
+		for ($i = $start; $i < $end; $i++)
+		{
+			if ($i > $start)
+				{
+					$result .= " ";
+				}
+			
+			if (($i % 3) == 0) 
+			{
+				$result .= "Fizz";
+			} 
+			if (($i % 5) == 0) 
+			{
+				$result .= "Buzz";
+			}
+			
+			if (($i % 3) != 0 && ($i % 5) != 0)
+			{
+				
+				$result .= $i;
+			}
+		}
+		echo $result;
+	}	
+}
+?>
