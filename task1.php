65fcf4ce17dde2594442ca0b2bc2ca9ffb8ce57e
Triaji Putranto <foreward@gmail.com>
2013-05-03 21:15:01 +0800

    Task 1 Question 2: adding the bazz
    To capture when the Fizz and Buzz is being called consecutively,
    counter called consecutive is made to count when either the Buzz or the
    Fizz has been called consecutively. If the conter reaches 2 means that
    the Fizz or Buzz have been called 2 times consecutively, so the next
    item would be changed to Bazz. If the Bazz or a normal integer is
    printed, then the consecutive counter is reset to 0 to capture the next
    consecutive call

--- task1.php
+++ task1.php
@@ -32,6 +32,7 @@ function buzzFizz($startRange = 0, $endRange = 0){
 			$end = $startRange;
 		}
 		$result = "";
+		$consecutive = 0;
 		for ($i = $start; $i <= $end; $i++)
 		{
 			if ($i > $start)
@@ -42,16 +43,27 @@ function buzzFizz($startRange = 0, $endRange = 0){
 			if (($i % 3) == 0) 
 			{
 				$result .= "Fizz";
+				$consecutive++;
 			} 
 			if (($i % 5) == 0) 
 			{
 				$result .= "Buzz";
+				$consecutive++;
 			}
 			
 			if (($i % 3) != 0 && ($i % 5) != 0)
 			{
+				if ($consecutive == 2) 
+				{
+					$result .= "Bazz";
+					$consecutive = 0;
+				}
+				else
+				{
+					$result .= $i;
+					$consecutive = 0;
+				}
 				
-				$result .= $i;
 			}
 		}
 		echo $result;
